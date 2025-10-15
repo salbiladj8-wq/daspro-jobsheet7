1184dcf22377acd2e9c25df5cf97d9bb91e3bace
Salbila <salbiladj8@gmail.com>
2025-10-15 12:55:36 +0700

    tugas3

---
+++ KafeDoWhile28.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class KafeDoWhile28 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kopi;
+        int teh;
+        int roti;
+        String namaPelanggan;
+        int hargaKopi = 12000;
+        int hargaTeh = 7000;
+        int hargaRoti = 200000;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+            int totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total harga yang harus dibayar Rp: " + totalHarga);
+            sc.nextLine();
+        } while (true);
+
+        System.out.println("Semua transaksi selesai.");
+    }
+}
