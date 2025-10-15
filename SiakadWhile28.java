57c52725e55c657912393583310ef22af5fc1417
Salbila <salbiladj8@gmail.com>
2025-10-15 12:50:13 +0700

    modiftugas2

--- SiakadWhile28.java
+++ SiakadWhile28.java
@@ -16,6 +16,7 @@ public class SiakadWhile28 {
             }
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " : A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " : B+");
             } else if (nilai > 65 && nilai <= 73) {
